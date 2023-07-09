3fb30de4f89ebc002efa776c9b6e75ef7af0a6ad
Joe Buehl <joe@thebuehls.com>
2023-07-09 14:50:35 -0700

    Add C or F scale option

--- homealone/interfaces/tc74Interface.py
+++ homealone/interfaces/tc74Interface.py
@@ -3,8 +3,9 @@ from homealone import *
 # TC74 temp sensor
 
 class TC74Interface(Interface):
-    def __init__(self, name, interface):
+    def __init__(self, name, interface, scale="F"):
         Interface.__init__(self, name, interface)
+        self.scale = scale.upper()
 
     def read(self, addr):
         debug('debugTemp', self.name, "read", addr)
@@ -12,6 +13,8 @@ class TC74Interface(Interface):
             value = self.interface.read((addr, 0))
             if value > 127:
                 value = (256-value) * (-1)
-            return float(value) * 9 / 5 + 32
+            if self.scale == "F":
+                value = float(value) * 9 / 5 + 32
+            return value
         except:
             return 0
