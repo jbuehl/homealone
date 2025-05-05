1aadd43da9a9d3d29a0561d35bcc986cfa4d9248
Joe Buehl <joe@thebuehls.com>
2025-05-05 15:19:10 -0700

    Exception handling

--- homealone/interfaces/w1Interface.py
+++ homealone/interfaces/w1Interface.py
@@ -5,6 +5,9 @@ from homealone import *
 class W1Interface(Interface):
     def __init__(self, name, interface=None, event=None):
         Interface.__init__(self, name, interface=interface, event=event)
+        self.getSensors()
+
+    def getSensors(self):
         self.sensors = {}
         for sensor in W1ThermSensor.get_available_sensors():
             self.sensors[sensor.id.upper()] = sensor
@@ -12,10 +15,9 @@ class W1Interface(Interface):
     def read(self, addr):
         try:
             return float(self.sensors[addr.upper()].get_temperature()) * 9 / 5 + 32
-        except KeyError:
+        except KeyError:                        # sensor wasn't previously detected
             return 0
-        except errors.SensorNotReadyError:    # sensor isn't responding
-            self.sensors = {}
-            for sensor in W1ThermSensor.get_available_sensors():
-                self.sensors[sensor.id] = sensor
+        except errors.SensorNotReadyError:      # sensor isn't responding
+            log(self.name, addr, "not responding")
+            self.getSensors()
             return 0
