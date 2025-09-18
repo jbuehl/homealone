367f80b09227716d12b958f174193d05fc663f76
Joe Buehl <joe@thebuehls.com>
2025-09-18 10:22:56 -0700

    Cache sensor states

--- homealone/interfaces/osInterface.py
+++ homealone/interfaces/osInterface.py
@@ -8,11 +8,15 @@ class OSInterface(Interface):
     def __init__(self, name, interface=None, event=None):
         Interface.__init__(self, name, interface=interface, event=event)
 
-    def read(self, addr):
-        addrParts = addr.split()
-        if addrParts[0] == "cpuTemp":
-            return float(subprocess.check_output("vcgencmd measure_temp", shell=True)[5:-3])
-        elif addrParts[0] == "cpuLoad":
+    def start(self, notify=None):
+        startThread(name="readStateThread", target=self.readStates, notify=notify)
+
+    # thread to constantly read and cache values
+    def readStates(self):
+        while True:
+            # CPU temp
+            self.states["cpuTemp"] = float(subprocess.check_output("vcgencmd measure_temp", shell=True)[5:-3])
+            # CPU load
             with open('/proc/stat') as f:
                 fields = [float(column) for column in f.readline().strip().split()[1:]]
             last_idle, last_total = fields[3], sum(fields)
@@ -22,21 +26,22 @@ class OSInterface(Interface):
             idle, total = fields[3], sum(fields)
             idle_delta, total_delta = idle - last_idle, total - last_total
             last_idle, last_total = idle, total
-            return 100.0 * (1.0 - idle_delta / total_delta)
-        elif addrParts[0] == "diskUse":
-            useParts = subprocess.check_output("df "+addrParts[1], shell=True).decode().split("\n")[1].split()
-            return 100 * int(useParts[2]) / (int(useParts[2]) + int(useParts[3]))
-        elif addrParts[0] == "ssid":
+            self.states["cpuLoad"] = 100.0 * (1.0 - idle_delta / total_delta)
+            # disk usage of the root filesystem
+            useParts = subprocess.check_output("df /", shell=True).decode().split("\n")[1].split()
+            self.states["diskUse"] = 100 * int(useParts[2]) / (int(useParts[2]) + int(useParts[3]))
+            # IP address
             try:
-                return subprocess.check_output("iwconfig "+addrParts[1]+"|grep ESSID", shell=True).decode().strip("\n").split(":")[-1].split("/")[0].strip().strip('"')
+                self.states["ipAddr"] = subprocess.check_output("ifconfig|grep inet\ |grep -v 127", shell=True).decode().strip("\n").split()[1]
             except subprocess.CalledProcessError:
-                return ""
-        elif addrParts[0] == "ipAddr":
-            try:
-                return subprocess.check_output("ifconfig|grep inet\ |grep -v 127", shell=True).decode().strip("\n").split()[1]
-            except subprocess.CalledProcessError:
-                return ""
-        elif addrParts[0] == "uptime":
-            return " ".join(c for c in subprocess.check_output("uptime", shell=True).decode().strip("\n").split(",")[0].split()[2:])
-        else:
+                self.states["ipAddr"] = ""
+            # uptime
+            self.states["uptime"] = " ".join(c for c in subprocess.check_output("uptime", shell=True).decode().strip("\n").split(",")[0].split()[2:])
+
+            time.sleep(10)
+
+    def read(self, addr):
+        try:
+            return self.states[addr]
+        except KeyError:
             return None
--- homealone/interfaces/w1Interface.py
+++ homealone/interfaces/w1Interface.py
@@ -7,19 +7,29 @@ from homealone import *
 class W1Interface(Interface):
     def __init__(self, name, interface=None, event=None):
         Interface.__init__(self, name, interface=interface, event=event)
+
+    def start(self, notify=None):
         self.getSensors()
+        startThread(name="readStateThread", target=self.readStates, notify=notify)
 
     def getSensors(self):
         self.sensors = {}
         for sensor in W1ThermSensor.get_available_sensors():
-            self.sensors[sensor.id.upper()] = sensor
+            self.sensors[sensor.id.lower()] = sensor
+
+    # thread to constantly read and cache values
+    def readStates(self):
+        while True:
+            for sensor in list(self.sensors.keys()):
+                try:
+                    self.states[sensor.lower()] = float(self.sensors[sensor].get_temperature()) * 9 / 5 + 32
+                except KeyError:                        # sensor wasn't previously detected
+                    self.states[sensor.lower()] = None
+                except errors.SensorNotReadyError:      # sensor isn't responding
+                    log(self.name, sensor, "not responding")
+                    self.getSensors()
+                    self.states[sensor.lower()] = None
+                time.sleep(1)
 
     def read(self, addr):
-        try:
-            return float(self.sensors[addr.upper()].get_temperature()) * 9 / 5 + 32
-        except KeyError:                        # sensor wasn't previously detected
-            return None
-        except errors.SensorNotReadyError:      # sensor isn't responding
-            log(self.name, addr, "not responding")
-            self.getSensors()
-            return None
+        return self.states[addr.lower()]
